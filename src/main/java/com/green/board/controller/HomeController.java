f7a4e8107e5ec5d5fedbbabe024827a88af41d3f
ssm512 <shinsang512@naver.com>
2026-04-22 15:05:24 +0900

    Board 설정 완료

---
+++ src/main/java/com/green/board/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.green.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public	String	home() {
+		return "home";
+	}
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String	test() {
+		return "<h2>Test 입니다</h2>";
+	}
+	
+}
